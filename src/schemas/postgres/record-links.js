da4450eda0479d79e9f020c1104166fdf77a5fcd
Todd Runstein <toddrun@gmail.com>
2022-08-10 15:22:15 -0700

    Add record-links table

---
+++ src/schemas/postgres/record-links.js
@@ -0,0 +1,46 @@
+import TableDefinition from '../../table-definition';
+
+export default class RecordLinks extends TableDefinition {
+  get name() {
+    return 'record_links';
+  }
+
+  defineTable() {
+    this.pk('id', {});
+    this.integer('row_id', {'allowNull': false});
+    this.string('row_resource_id', {'allowNull': false});
+    this.string('key', {'allowNull': false});
+    this.integer('form_id', {'allowNull': false});
+    this.string('form_resource_id', {'allowNull': false});
+    this.integer('record_id', {});
+    this.string('record_resource_id', {});
+    this.integer('linked_form_id', {});
+    this.string('linked_form_resource_id', {});
+    this.integer('linked_record_id', {});
+    this.string('linked_record_resource_id', {});
+    this.timestamp('created_at', {'allowNull': false});
+    this.timestamp('updated_at', {'allowNull': false});
+  }
+
+  defineView() {
+    this.alias('row_resource_id', 'record_link_id');
+    this.alias('key', 'key');
+    this.alias('form_resource_id', 'form_id');
+    this.alias('record_resource_id', 'record_id');
+    this.alias('linked_form_resource_id', 'linked_form_id');
+    this.alias('linked_record_resource_id', 'linked_record_id');
+    this.alias('created_at', 'created_at');
+    this.alias('updated_at', 'updated_at');
+  }
+
+  defineIndexes() {
+    this.index({'columns': [ 'row_resource_id' ], 'unique': true});
+    this.index({'columns': [ 'row_id' ], 'unique': true});
+    this.index({'columns': [ 'form_id', 'key' ]});
+    this.index({'columns': [ 'form_resource_id', 'record_resource_id' ]});
+    this.index({'columns': [ 'linked_form_resource_id' ]});
+    this.index({'columns': [ 'linked_form_resource_id', 'linked_record_resource_id' ]});
+    this.index({'columns': [ 'record_resource_id' ]});
+    this.index({'columns': [ 'linked_record_resource_id' ]});
+  }
+}
